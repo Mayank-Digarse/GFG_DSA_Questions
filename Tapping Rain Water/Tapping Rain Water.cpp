00358ea19a6caa8b95856f602eeab1daea60da39
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2022-10-22 21:35:57 +0530

    Create Tapping Rain Water.cpp

---
+++ Tapping Rain Water/Tapping Rain Water.cpp
@@ -0,0 +1,25 @@
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+       int left[n] ;
+       int right[n];
+       int max1 = arr[0];
+       int max2 = arr[n-1];
+       for(int i =0; i<n; i++){
+           max1 = max(arr[i] , max1);
+           left[i] = max1;
+           
+           max2 = max(arr[(n-i)-1] , max2);
+           right[(n-i)-1] = max2;
+           
+       }
+       long long sum = 0;
+       for(int i = 0; i<n; i++){
+           sum += abs(arr[i] - min(left[i] , right[i]));
+       }
+        return sum;
+        
+    }
+};
