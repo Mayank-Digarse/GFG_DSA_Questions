983f5b952d2dd4c87aaab4e8a540aadedb60fd23
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2022-10-23 10:54:15 +0530

    Create Palindrome String.cpp

---
+++ Palindrome String/Palindrome String.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int i = 0;
+	    int j = S.size()-1;
+	    while(i<j){
+	        if(S[i++] != S[j--])
+	            return 0;
+	        
+	        
+	    }
+	    return 1;
+	}
+
+};
