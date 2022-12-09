eb81c624c50a503b7506ff770cd8dee623464c24
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2022-12-09 14:58:06 +0530

    Create Print Anagrams Together.cpp

---
+++ Print Anagrams Together/Print Anagrams Together.cpp
@@ -0,0 +1,22 @@
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        unordered_map<string ,vector<int>>mp;
+        int n = string_list.size();
+        for(int i = 0; i<n; i++){
+            string curr = string_list[i];
+            sort(curr.begin() , curr.end());
+            mp[curr].push_back(i);
+        }
+        vector<vector<string>>ans;
+        for(auto &e : mp){
+            vector<string>curr;
+            for(auto &i : e.second){
+                curr.push_back(string_list[i]);
+            }
+            ans.push_back(curr);
+        }
+        return ans;
+    }
+};
